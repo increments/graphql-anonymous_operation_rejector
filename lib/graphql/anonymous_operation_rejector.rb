17e0054422921c5513ceff90b8cb9b414813d66e
Yuku TAKAHASHI <taka84u9@gmail.com>
2018-09-21 13:17:47 +0900

    Implement

--- lib/graphql/anonymous_operation_rejector.rb
+++ lib/graphql/anonymous_operation_rejector.rb
@@ -1,7 +1,37 @@
+require 'graphql'
+
 require 'graphql/anonymous_operation_rejector/version'
 
-module Graphql
-  module AnonymousOperationRejector
-    # Your code goes here...
+module GraphQL
+  # An GraphQL analyzer to reject anonymous operation (query without operation name).
+  #
+  # @see http://facebook.github.io/graphql/June2018/#sec-Anonymous-Operation-Definitions
+  class AnonymousOperationRejector
+    def analyze?(_query)
+      true
+    end
+
+    # @param query [GraphQL::Query]
+    # @return [Hash]
+    def initial_value(query)
+      { anonymous: anonymous_query?(query) }
+    end
+
+    def call(memo, _visit_type, _irep_node)
+      memo
+    end
+
+    def final_value(memo)
+      memo[:anonymous] &&
+        GraphQL::AnalysisError.new('Anonymous operation is not allowed. Please add operation name to your query.')
+    end
+
+    private
+
+    # @param query [GraphQL::Query]
+    # @return [true, false]
+    def anonymous_query?(query)
+      query.operations.values.any? { |operation| operation.name.nil? }
+    end
   end
 end
--- spec/graphql/anonymous_operation_rejector_spec.rb
+++ spec/graphql/anonymous_operation_rejector_spec.rb
@@ -1,9 +1,34 @@
-RSpec.describe Graphql::AnonymousOperationRejector do
+require 'graphql'
+
+RSpec.describe GraphQL::AnonymousOperationRejector do
   it 'has a version number' do
-    expect(Graphql::AnonymousOperationRejector::VERSION).not_to be nil
+    expect(GraphQL::AnonymousOperationRejector::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  let(:schema) do
+    query_type = Class.new(GraphQL::Schema::Object) do
+      graphql_name 'Query'
+      field :hello, String, null: false
+
+      def hello
+        'world'
+      end
+    end
+
+    Class.new(GraphQL::Schema) do
+      query_analyzer GraphQL::AnonymousOperationRejector.new
+      query query_type
+    end
+  end
+
+  it 'allows GraphQL query with operation name' do
+    result = schema.execute('query operationName { hello }')
+    expect(result.to_h['data']).to eq('hello' => 'world')
+  end
+
+  it 'rejects GraphQL query without operation name' do
+    result = schema.execute('{ hello }')
+    message = 'Anonymous operation is not allowed. Please add operation name to your query.'
+    expect(result.to_h['errors']).to include(a_hash_including('message' => message))
   end
 end
